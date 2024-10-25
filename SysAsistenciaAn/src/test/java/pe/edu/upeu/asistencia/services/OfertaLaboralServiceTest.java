8a1f0cd4cf589faff0c220751e92c79e3e796822
tu-nombre-de-usuario <braynermc2021@gmail.com>
2024-10-24 21:41:33 -0500

    brayner888877777

--- SysAsistenciaAn/src/test/java/pe/edu/upeu/asistencia/services/OfertaLaboralServiceTest.java
+++ SysAsistenciaAn/src/test/java/pe/edu/upeu/asistencia/services/OfertaLaboralServiceTest.java
@@ -39,7 +39,7 @@ public class OfertaLaboralServiceTest {
 
     @Test
     public void testGetOfertaById() {
-        // Simulamos que el repositorio devuelve una oferta laboral
+       
         given(ofertaLaboralRepository.findById(1L)).willReturn(Optional.of(ofertaLaboral));
 
         OfertaLaboral result = ofertaLaboralService.findById(1L);
@@ -51,7 +51,7 @@ public class OfertaLaboralServiceTest {
 
     @Test
     public void testCreateOferta() {
-       
+
         given(ofertaLaboralRepository.save(Mockito.any(OfertaLaboral.class))).willReturn(ofertaLaboral);
 
         OfertaLaboral result = ofertaLaboralService.save(ofertaLaboral);
